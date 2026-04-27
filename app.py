75fb36e1e7ea23a636f6a628f42d1a373dfeabd9
sagarrvm89 <159737045+sagarrvm89@users.noreply.github.com>
2026-04-26 23:36:36 -0700

    Add multiple job routes and privacy page

--- app.py
+++ app.py
@@ -7,6 +7,26 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/resume-analyzer")
+def resume_analyzer():
+    return render_template("analyzer.html")
+
+@app.route("/adzuna-jobs")
+def adzuna_jobs():
+    return render_template("adzuna.html")
+
+@app.route("/jooble-jobs")
+def jooble_jobs():
+    return render_template("jooble.html")
+
+@app.route("/dice-jobs")
+def dice_jobs():
+    return render_template("dice.html")
+
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html")
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
